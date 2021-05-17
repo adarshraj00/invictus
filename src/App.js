c7233c8c8c25cb9d092a3600e962849d1196a4e8
Adarsh Raj <43468326+adarshraj00@users.noreply.github.com>
2021-05-17 11:51:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./styles.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default function App() {
+  let [sortedhash, set] = useState([]);
+  let [n, setn] = useState(0);
+  let [t, sett] = useState(0);
+  const res = sortedhash.slice(0, t);
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/invictustech/test/main/README.md")
       .then((res) => {
@@ -9,32 +13,46 @@ export default function App() {
       .then((res) => {
         res.read().then((data, flag) => {
           var str = String.fromCharCode.apply(null, data.value);
-          //  console.log(str.split('.').join(' ').split(',').join(' ').split(';').join(' ').split('-').join(' ').split(' '));
-          console.log(
-            str
-              .split(" ")
-              .join(".")
-              .split(".")
-              .filter((s) => {
-                return s.length >= 1;
-              })
-              .map((s) => {
-                let c = s[s.length - 1];
-                return c === "," || c === ";" || c === "-"
-                  ? s.slice(0, s.length - 1)
-                  : s;
-              })
-          );
-          console.log(str);
+          let temp = "",
+            hash = {};
+          for (let i = 0; i < str.length; i++) {
+            if (
+              (str[i] >= "a" && str[i] <= "z") ||
+              (str[i] >= "A" && str[i] <= "Z")
+            ) {
+              temp += str[i];
+            } else if (temp !== "") {
+              if (hash[temp]) {
+                hash[temp]++;
+              } else {
+                hash[temp] = 1;
+              }
+              temp = "";
+            }
+          }
+          if (temp !== "") {
+            if (hash[temp]) hash[temp]++;
+            else hash[temp] = 1;
+          }
+          let sortedhash = Object.entries(hash).sort((a, b) => b[1] - a[1]);
+          set(sortedhash);
         });
       });
-  });
+  }, []);
   return (
     <div className="App">
       <h1>
-        <input></input>
+        <input value={n} onChange={(e) => setn(e.target.value)}></input>
       </h1>
-      <button>submit</button>
+      <button onClick={() => sett(n)}>submit</button>
+      <br />
+      {res.map((a) => {
+        return (
+          <>
+            {a[0]} {a[1]} <br />
+          </>
+        );
+      })}
     </div>
   );
 }
